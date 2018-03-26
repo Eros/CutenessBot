fa115532663f27dea1730504e67686ca11537a84
Eros <george.marr.cobc@gmail.com>
2018-03-26 13:58:24 +0100

    fuckign python

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 import logging; logging.basicConfig(level = logging.INFO, format = '%(asctime)s %(levelname)s: %message', datefmt='%Y-%M-%d %H:$M:$S')
 
 class TwitterAssistant:
+
     from birdy.twitter import StreamClient
     from birdy.twitter import UserClient
 
@@ -15,3 +16,20 @@ class TwitterAssistant:
     ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
     ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 
+    streaming_client = StreamClient(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    rest_client = UserClient(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    def permaLink(tweetID, screenName):
+        return 'https://twitter.com/' + screenName + '/statuses/' + tweetID
+
+    def recentTweets(count, screenName = None):
+        return TwitterAssistant.streaming_client.api.statuses_user_timeline.get(screenName = screenName, count = count).data
+
+    def rawStream(rawSearchTerms):
+        return TwitterAssistant.streaming_client.stream.statuses.filter.post(track = rawSearchTerms).stream()
+
+    def getTweet(tweetID):
+        return TwitterAssistant.rest_client.api.statuses.show.get(id = tweetID).data
+
+    def sendTweet(reply, text):
+        TwitterAssistant.rest_client.api.statuses.update.post(reply = reply, status = text)
